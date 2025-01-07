a49a67cdbfdf118ac38a637d42e878ffc060f597
kamal126 <54989946+kamal126@users.noreply.github.com>
2025-01-07 13:11:29 +0530

    fun with math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+const num = 123.8966
+
+// console.log(num.toPrecision(4));
+// console.log(num.toPrecision(3));
+
+const Billinior = 500000000
+// console.log(Billinior.toLocaleString('en-IN'));
+
+// +++++++++++++++++ Math +++++++++++++++++++++
+
+// console.log(Math);  // Object [Math] {}
+// console.log(Math.abs(-4));  // 4
+// console.log(Math.round(4.6));  // 5
+// console.log(Math.ceil(4.2));  // 5
+// console.log(Math.floor(4.9));  // 4
+// console.log(Math.min(6,3,5,7,));  // 3
+// console.log(Math.max(0,8,4,2,5,9));  // 9
+
+const num1 =Math.random()
+console.log(num1);
+// console.log((Math.ceil(num1)));
+console.log((Math.floor(num1)*10+1));
+
+const min = 10, max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
