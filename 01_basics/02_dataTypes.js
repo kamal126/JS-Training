8430a4303d5dd174b040512a73b3260a8e304cdf
kamal126 <54989946+kamal126@users.noreply.github.com>
2025-01-07 11:41:49 +0530

    data types disscuss

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3
+    +
+    3
+) //code readability should be high
+
+let name = "kamal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/**
+ * number => 2 to power 53 => 2^53
+ * bigInt
+ * string => ""
+ * boolean=> true/false
+ * null => standalone value
+ * undefined =>
+ * symbol => unique
+ */
+
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   //object
+
+
+
