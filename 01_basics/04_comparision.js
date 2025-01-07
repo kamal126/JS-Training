56cee9e3ae3443c43faba595c49a0e3ed2e2996d
kamal126 <54989946+kamal126@users.noreply.github.com>
2025-01-07 12:10:44 +0530

    comparision discussion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+console.log(2 > 1);  // true
+console.log(2 >= 1);  // true
+console.log(2 < 1);  // false
+console.log(2 == 1);  // false
+console.log(2 != 1);  // true
+console.log("2" > 1);  // true
+console.log("02" == 2);  // true
+console.log(null > 0);  // false
+console.log(null == 0);  // false
+console.log(null >= 0);  // true
+console.log(undefined == 0);  //  false
+console.log(undefined > 0);  // false
+console.log(undefined < 0);  // false
+console.log(true==1);  // true
+console.log(false == 0);  // true
+console.log(false == "");  // true
+
+// strict comparision ( === )
+
+console.log("2" === 2);  // false
+console.log(true === 1);  // false
+console.log(false === "");  // false
+console.log("r" === 'r');  // true
+
+
+
+
